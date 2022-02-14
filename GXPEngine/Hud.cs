4f0215dd6d9e1ce354d7237045b945f48ed569b3
Tjalle Borgers <tjalle@borgersonline.nl>
2022-02-14 16:07:45 +0100

    Revert "Delete Hud.cs"
    This reverts commit 9b8edeb3fefbd1b82145d52bd0e860c77cd4aa06.

---
+++ GXPEngine/Hud.cs
@@ -0,0 +1,128 @@
+﻿using System.Drawing;
+using GXPEngine;
+class Hud : GameObject
+{
+    public int scoreCount = 0;
+    public string playerName = "";
+
+    //Hud elemets
+    EasyDraw fuelbarHudElement;
+    EasyDraw healthHudElement;
+    EasyDraw scoreHudElement;
+
+    Scene activeScene;
+    Sprite fuelbarFill;
+    float fuelbarFillAmount;
+    
+    public Hud(Scene pActiveScene, string pName) : base()
+    {
+        activeScene = pActiveScene;
+        playerName = pName;
+
+        //Create hud element for health
+        CreateHealthElement();
+
+        //Create hud element for fuel
+        CreateFuelElement();
+
+        //Create hud element for score
+        CreateScoreElement();
+    }
+
+    public void UpdateHealth(int healthBoost)
+    {
+        int playerHealth = activeScene.player.health + healthBoost; //New player health
+
+        foreach (Sprite child in healthHudElement.GetChildren()) //Empty old healthbar
+        {
+            healthHudElement.RemoveChild(child);
+        }
+
+        Sprite sprite;
+        for (int i = 1; i <= playerHealth; i++) //Create hearts for every liefepoint of Player. Empty hearts for the remaining difference to maxhealth
+        {
+            if (playerHealth >= i)
+            {
+                sprite = new Sprite("StaticHeart.png", false, false);
+            }
+            else
+            {
+                sprite = new Sprite("StaticHeartEmpty.png", false, false);
+            }
+            sprite.SetOrigin(sprite.width / 2, sprite.height / 2);
+            sprite.scale = CoreParameters.hudHealthScale;
+            sprite.SetXY(i * healthHudElement.width - healthHudElement.width / 2, healthHudElement.height);
+            healthHudElement.AddChild(sprite);
+        }
+    }
+
+    public void UpdateFuelbar(int pFuelbarBoost)
+    {
+        fuelbarFillAmount += pFuelbarBoost;
+    }
+
+    public void UpdateScore(int pScoreBoost)
+    {
+        scoreCount += pScoreBoost;
+    }
+
+    void CreateHealthElement()
+    {
+        healthHudElement = new EasyDraw(CoreParameters.hudHealthWidth, CoreParameters.hudHealthHeight, false);
+        healthHudElement.SetOrigin(healthHudElement.width / 2, healthHudElement.height / 2);
+        healthHudElement.SetXY(CoreParameters.hudHealthPosX, CoreParameters.hudHealthPosY);
+        AddChild(healthHudElement);
+        UpdateHealth(0);
+    }
+
+    void CreateFuelElement()
+    {
+        fuelbarHudElement = new EasyDraw(CoreParameters.hudFuelWidth, CoreParameters.hudFuelHeight, false);
+        fuelbarHudElement.SetOrigin(fuelbarHudElement.width / 2, fuelbarHudElement.height / 2);
+        fuelbarHudElement.SetXY(CoreParameters.hudFuelPosX, CoreParameters.hudFuelPosY);
+        fuelbarFill = new Sprite("fuelbar.png", false, false);
+        fuelbarFill.SetOrigin(fuelbarFill.width / 2, fuelbarFill.height / 2);
+        fuelbarFill.scale = CoreParameters.hudFuelbarScale;
+        fuelbarFill.SetXY(fuelbarHudElement.width, fuelbarHudElement.height);
+        fuelbarFill.width = Mathf.Round(100*(Time.time - activeScene.lastFuel)/CoreParameters.maxTimeBetweenFuel);
+        fuelbarHudElement.AddChild(fuelbarFill);
+        AddChild(fuelbarHudElement);
+        fuelbarFillAmount = fuelbarFill.width;
+    }
+
+    void CreateScoreElement()
+    {
+        scoreHudElement = new EasyDraw(CoreParameters.hudScoreWidth, CoreParameters.hudScoreHeight, false);
+        scoreHudElement.SetXY(CoreParameters.hudScorePosX, CoreParameters.hudScorePosY);
+        Font font = Utils.LoadFont(CoreParameters.fontPath, CoreParameters.hudScoreFontSize);
+        scoreHudElement.TextFont(font);
+        scoreHudElement.TextAlign(CenterMode.Min, CenterMode.Center);
+        scoreHudElement.Fill(Color.White);
+        AddChild(scoreHudElement);
+        UpdateScore(0);
+    }
+
+    void Update()
+    {
+        int clampedDeltaTime = Mathf.Min(Time.deltaTime, 40);
+
+        //Score update
+        //scoreCount++;
+        scoreHudElement.Text(activeScene.score.ToString(), true);
+
+        //Fuel update
+        if (fuelbarFill.width > 0) {
+            int oldFillAmount = (int)fuelbarFillAmount;
+            fuelbarFillAmount -= CoreParameters.hudFuelbarLooseOverTime * clampedDeltaTime;
+            if (oldFillAmount != (int)fuelbarFillAmount)
+            {
+                fuelbarFill.width = (int)fuelbarFillAmount;
+                fuelbarFill.x = oldFillAmount - fuelbarFill.width / 2;
+
+            }
+        } else if (fuelbarFill.width < 0)
+        {
+            fuelbarFill.width = 0;
+        }
+    }
+}
